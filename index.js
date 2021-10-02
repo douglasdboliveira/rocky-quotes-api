915f16ae0a22156f413d9daa9a0d5c1b931e8b3a
dougdboliveira <douglasdboliveira@gmail.com>
2021-10-01 23:54:16 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const quotes = require('./quotes');
+const app = express();
+
+app.get("/quotes", (req, res) => {
+    res.json(quotes);
+});
+
+app.get("/random", (req, res) => {
+    res.send(quotes[Math.floor(Math.random() * 35)]);
+});
+
+app.get("/quotes/:value", (req, res) => {
+    const value = req.params.value - 1;
+
+    res.send(quotes[value]);
+});
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000!");
+});
---
+++ quotes.js
@@ -0,0 +1,109 @@
+const quotes = [
+    {
+        "1": "Going in one more round when you don’t think you can. That’s what makes all the difference in your life."
+    },
+    {
+        "2": "Until you start believing in yourself, you ain’t gonna have a life."
+    },
+    {
+        "3": "Every champion was once a contender who refused to give up."
+    },
+    {
+        "4": "If I can change and you can change, everybody can change."
+    },
+    {
+        "5": "Let me tell you something you already know. The world ain’t all sunshine and rainbows. It’s a very mean and nasty place and I don’t care how tough you are it will beat you to your knees and keep you there permanently if you let it. You, me, or nobody is gonna hit as hard as life. But it ain’t about how hard ya hit. It’s about how hard you can get hit and keep moving forward. How much you can take and keep moving forward. That’s how winning is done!"
+    },
+    {
+        "6": "Now, if you know what you’re worth, then go out and get what you’re worth. But you gotta be willing to take the hits, and not pointing fingers saying you ain’t where you wanna be because of him, or her, or anybody. Cowards do that and that ain’t you. You’re better than that!"
+    },
+    {
+        "7": "No, maybe I can’t win, maybe the only thing I can do is just take everything he’s got. But to beat me, he’s gonna have to kill me, and to kill me, he’s gonna have to have the heart to stand in front of me, and to do that, he’s gotta be willing to die himself and I don’t know if he’s ready to do that. I don’t know, I don’t know."
+    },
+    {
+        "8": "Nobody owes nobody nothing. You owe yourself."
+    },
+    {
+        "9": "Because if you’re willing to go through all the battling you got to go through to get where you want to get, who’s got the right to stop you? I mean maybe some of you guys got something you never finished, something you really want to do, something you never said to someone, something… and you’re told no, even after you paid your dues? Who’s got the right to tell you that, who? Nobody! It’s your right to listen to your gut, it ain’t nobody’s right to say no after you earned the right to be where you want to be and do what you want to do!"
+    },
+    {
+        "10": "Remember, the mind is your best muscle. Big arms can move rocks, but big words can move mountains."
+    },
+    {
+        "11": "Ya know, they always say if you live in one place long enough, you are that place."
+    },
+    {
+        "12": "To beat me, he’s going to have to kill me. He’s gonna have to have the heart to stand in front of me and to do that, he’s got to be willing to die himself."
+    },
+    {
+        "13": "If this is something you wanna do, and if this is something you gotta do, then you do it. Fighters fight."
+    },
+    {
+        "14": "Time takes everybody out. Time is undefeated."
+    },
+    {
+        "15": "I believe there’s an inner power that makes winners or losers. And the winners are the ones who really listen to the truth of their hearts."
+    },
+    {
+        "16": "C’mon, champ, hit me in the face! My mom hits harder than you!"
+    },
+    {
+        "17": "I stopped thinking the way other people think a long time ago. You gotta think like you think."
+    },
+    {
+        "18": "Every once in a while a person comes along who defies the odds, who defies logic, and fulfills an incredible dream."
+    },
+    {
+        "19": "You’re gonna have to go through hell, worse than any nightmare you’ve ever dreamed. But when it’s over, I know you’ll be the one standing. You know what you have to do. Do it."
+    },
+    {
+        "20": "It ain’t about how hard you hit. It’s about how hard you can get hit and keep moving forward. How much you can take and keep moving forward. That’s how winning is done!"
+    },
+    {
+        "21": "I am not the richest, smartest or most talented person in the world. But I succeed because I keep going and going and going."
+    },
+    {
+        "22": "The toughest opponent you’re ever going to have to face (is in the mirror)."
+    },
+    {
+        "23": "You, me or nobody is gonna hit as hard as life."
+    },
+    {
+        "24": "Maybe it’s you who ain’t got it no more."
+    },
+    {
+        "25": "People do because they want to do."
+    },
+    {
+        "26": "Nothing is real if you don’t believe in who you are."
+    },
+    {
+        "27": "There’s a beast inside me."
+    },
+    {
+        "28": "You know they always say, if you live in one place long enough, you are that place."
+    },
+    {
+        "29": "When things got hard you started looking for something to blame, like a shadow."
+    },
+    {
+        "30": "All I wanna do is go the distance."
+    },
+    {
+        "31": "You can’t learn when you’re talking. That’s a fact of life. As long as you’re talking, you’re not listening."
+    },
+    {
+        "32": "Nothing’s over ’til it’s over."
+    },
+    {
+        "33": "Time takes everybody out. Time is undefeated."
+    },
+    {
+        "34": "I’m pursuing something and nobody looks too happy about it."
+    },
+    {
+        "35": "That’s the way I’m made."
+    }
+];
+
+module.exports = quotes;
